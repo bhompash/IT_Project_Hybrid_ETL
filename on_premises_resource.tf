41be43b527365a540e7fed09c8a2d88f9219e8fb
srikanth.bhompally33@gmail.com <surinenipurnachander@gmail.com>
2023-09-26 03:31:40 -0400

    creating resources in aws

--- on_premises_resource.tf
+++ on_premises_resource.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-st-track1"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
